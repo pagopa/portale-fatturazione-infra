0f1ee3c2be98f95791ad99ac51e32a78ba84967a
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2023-11-17 17:22:34 +0100

    Update src/azuredevops/core/99_variables.tf

--- src/azuredevops/core/99_variables.tf
+++ src/azuredevops/core/99_variables.tf
@@ -1,8 +1,6 @@
 locals {
   tenant_id = data.azurerm_client_config.current.tenant_id
 
-  #tfsec:ignore:GEN002
-  tlscert_renew_token = "v2"
 
   # TODO azure devops terraform provider does not support SonarCloud service endpoint
   azuredevops_serviceendpoint_sonarcloud_id = "9182be64-d387-465d-9acc-e79e802910c8"
