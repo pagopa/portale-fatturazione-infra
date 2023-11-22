391a29915112125dd10c46a5cad11c74e33069f3
pasqualedevita <>
2023-11-22 10:28:22 +0100

    feat: Add api.portalefatturazione.pagopa.it

--- src/azuredevops/core/10_tlscert_portalefatturazione-pagopa-it.tf
+++ src/azuredevops/core/10_tlscert_portalefatturazione-pagopa-it.tf
@@ -1,4 +1,4 @@
-variable "prod-tlscert-portalefatturazione-pagopa-it" {
+variable "tlscert-portalefatturazione-pagopa-it" {
   default = {
     repository = {
       organization   = "pagopa"
@@ -23,7 +23,7 @@ variable "prod-tlscert-portalefatturazione-pagopa-it" {
 }
 
 locals {
-  prod-tlscert-portalefatturazione-pagopa-it = {
+  tlscert-portalefatturazione-pagopa-it = {
     # TODO use var.location, but currently italy north region does not support federated identities
     location                            = "westeurope"
     tenant_id                           = local.tenant_id
@@ -37,17 +37,17 @@ locals {
       module.tls_cert_service_conn_prod.service_endpoint_id,
     ]
   }
-  prod-tlscert-portalefatturazione-pagopa-it-variables = {
+  tlscert-portalefatturazione-pagopa-it-variables = {
     KEY_VAULT_SERVICE_CONNECTION = module.tls_cert_service_conn_prod.service_endpoint_name,
     KEY_VAULT_NAME               = local.prod.key_vault_name
   }
-  prod-tlscert-portalefatturazione-pagopa-it-variables_secret = {
+  tlscert-portalefatturazione-pagopa-it-variables_secret = {
   }
 }
 
 module "tlscert-portalefatturazione-pagopa-it-cert_az" {
   depends_on = [module.letsencrypt_prod]
-  count      = var.prod-tlscert-portalefatturazione-pagopa-it.pipeline.enable_tls_cert == true ? 1 : 0
+  count      = var.tlscert-portalefatturazione-pagopa-it.pipeline.enable_tls_cert == true ? 1 : 0
 
   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_tls_cert_federated?ref=v4.1.1"
   providers = {
@@ -55,44 +55,44 @@ module "tlscert-portalefatturazione-pagopa-it-cert_az" {
   }
 
   project_id = data.azuredevops_project.project.id
-  location   = local.prod-tlscert-portalefatturazione-pagopa-it.location
-  repository = var.prod-tlscert-portalefatturazione-pagopa-it.repository
-  name       = var.prod-tlscert-portalefatturazione-pagopa-it.pipeline.dns_zone_name
+  location   = local.tlscert-portalefatturazione-pagopa-it.location
+  repository = var.tlscert-portalefatturazione-pagopa-it.repository
+  name       = trimprefix("${var.tlscert-portalefatturazione-pagopa-it.pipeline.dns_record_name}.${var.tlscert-portalefatturazione-pagopa-it.pipeline.dns_zone_name}", ".")
   #tfsec:ignore:general-secrets-no-plaintext-exposure
   #tfsec:ignore:GEN003
-  path                         = "${var.prefix}\\${var.prod-tlscert-portalefatturazione-pagopa-it.pipeline.path}"
+  path                         = "${var.prefix}\\${var.tlscert-portalefatturazione-pagopa-it.pipeline.path}"
   github_service_connection_id = azuredevops_serviceendpoint_github.azure-devops-github-ro.id
 
-  dns_record_name         = var.prod-tlscert-portalefatturazione-pagopa-it.pipeline.dns_record_name
-  dns_zone_name           = var.prod-tlscert-portalefatturazione-pagopa-it.pipeline.dns_zone_name
-  dns_zone_resource_group = local.prod-tlscert-portalefatturazione-pagopa-it.dns_zone_resource_group
-  tenant_id               = local.prod-tlscert-portalefatturazione-pagopa-it.tenant_id
-  subscription_name       = local.prod-tlscert-portalefatturazione-pagopa-it.subscription_name
-  subscription_id         = local.prod-tlscert-portalefatturazione-pagopa-it.subscription_id
+  dns_record_name         = var.tlscert-portalefatturazione-pagopa-it.pipeline.dns_record_name
+  dns_zone_name           = var.tlscert-portalefatturazione-pagopa-it.pipeline.dns_zone_name
+  dns_zone_resource_group = local.tlscert-portalefatturazione-pagopa-it.dns_zone_resource_group
+  tenant_id               = local.tlscert-portalefatturazione-pagopa-it.tenant_id
+  subscription_name       = local.tlscert-portalefatturazione-pagopa-it.subscription_name
+  subscription_id         = local.tlscert-portalefatturazione-pagopa-it.subscription_id
 
-  credential_key_vault_name           = local.prod-tlscert-portalefatturazione-pagopa-it.credential_key_vault_name
-  credential_key_vault_resource_group = local.prod-tlscert-portalefatturazione-pagopa-it.credential_key_vault_resource_group
+  credential_key_vault_name           = local.tlscert-portalefatturazione-pagopa-it.credential_key_vault_name
+  credential_key_vault_resource_group = local.tlscert-portalefatturazione-pagopa-it.credential_key_vault_resource_group
 
   variables = merge(
-    var.prod-tlscert-portalefatturazione-pagopa-it.pipeline.variables,
-    local.prod-tlscert-portalefatturazione-pagopa-it-variables,
+    var.tlscert-portalefatturazione-pagopa-it.pipeline.variables,
+    local.tlscert-portalefatturazione-pagopa-it-variables,
   )
 
   variables_secret = merge(
-    var.prod-tlscert-portalefatturazione-pagopa-it.pipeline.variables_secret,
-    local.prod-tlscert-portalefatturazione-pagopa-it-variables_secret,
+    var.tlscert-portalefatturazione-pagopa-it.pipeline.variables_secret,
+    local.tlscert-portalefatturazione-pagopa-it-variables_secret,
   )
 
-  service_connection_ids_authorization = local.prod-tlscert-portalefatturazione-pagopa-it.service_connection_ids_authorization
+  service_connection_ids_authorization = local.tlscert-portalefatturazione-pagopa-it.service_connection_ids_authorization
 
   schedules = {
-    days_to_build              = ["Thu"]
+    days_to_build              = ["Fri"]
     schedule_only_with_changes = false
-    start_hours                = 3
+    start_hours                = 6
     start_minutes              = 0
     time_zone                  = "(UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna"
     branch_filter = {
-      include = [var.prod-tlscert-portalefatturazione-pagopa-it.repository.branch_name]
+      include = [var.tlscert-portalefatturazione-pagopa-it.repository.branch_name]
       exclude = []
     }
   }
--- src/azuredevops/core/README.md
+++ src/azuredevops/core/README.md
@@ -34,6 +34,7 @@ Managed certificates are for hosts `dev.portalefatturazione.pagopa.it` and `port
 | <a name="module_letsencrypt_prod"></a> [letsencrypt\_prod](#module\_letsencrypt\_prod) | git::https://github.com/pagopa/terraform-azurerm-v3//letsencrypt_credential | v7.20.0 |
 | <a name="module_secret_core"></a> [secret\_core](#module\_secret\_core) | git::https://github.com/pagopa/terraform-azurerm-v3//key_vault_secrets_query | v7.20.0 |
 | <a name="module_tls_cert_service_conn_prod"></a> [tls\_cert\_service\_conn\_prod](#module\_tls\_cert\_service\_conn\_prod) | git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated | v4.0.0 |
+| <a name="module_tlscert-api-portalefatturazione-pagopa-it-cert_az"></a> [tlscert-api-portalefatturazione-pagopa-it-cert\_az](#module\_tlscert-api-portalefatturazione-pagopa-it-cert\_az) | git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_tls_cert_federated | v4.1.1 |
 | <a name="module_tlscert-portalefatturazione-pagopa-it-cert_az"></a> [tlscert-portalefatturazione-pagopa-it-cert\_az](#module\_tlscert-portalefatturazione-pagopa-it-cert\_az) | git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_tls_cert_federated | v4.1.1 |
 
 ## Resources
@@ -55,12 +56,13 @@ Managed certificates are for hosts `dev.portalefatturazione.pagopa.it` and `port
 | <a name="input_dev_subscription_name"></a> [dev\_subscription\_name](#input\_dev\_subscription\_name) | DEV Subscription name | `string` | n/a | yes |
 | <a name="input_location"></a> [location](#input\_location) | n/a | `string` | `""` | no |
 | <a name="input_prefix"></a> [prefix](#input\_prefix) | n/a | `string` | n/a | yes |
-| <a name="input_prod-tlscert-portalefatturazione-pagopa-it"></a> [prod-tlscert-portalefatturazione-pagopa-it](#input\_prod-tlscert-portalefatturazione-pagopa-it) | n/a | `map` | <pre>{<br>  "pipeline": {<br>    "dns_record_name": "",<br>    "dns_zone_name": "portalefatturazione.pagopa.it",<br>    "enable_tls_cert": true,<br>    "path": "TLS-Certificates",<br>    "variables": {<br>      "CERT_NAME_EXPIRE_SECONDS": "2592000"<br>    },<br>    "variables_secret": {}<br>  },<br>  "repository": {<br>    "branch_name": "refs/heads/master",<br>    "name": "le-azure-acme-tiny",<br>    "organization": "pagopa",<br>    "pipelines_path": "."<br>  }<br>}</pre> | no |
 | <a name="input_prod_dns_zone_rg_name"></a> [prod\_dns\_zone\_rg\_name](#input\_prod\_dns\_zone\_rg\_name) | PROD DNS Zone resource group name | `string` | n/a | yes |
 | <a name="input_prod_key_vault_name"></a> [prod\_key\_vault\_name](#input\_prod\_key\_vault\_name) | PROD Key Vault name | `string` | n/a | yes |
 | <a name="input_prod_key_vault_rg_name"></a> [prod\_key\_vault\_rg\_name](#input\_prod\_key\_vault\_rg\_name) | PROD Key Vault resource group name | `string` | n/a | yes |
 | <a name="input_prod_subscription_name"></a> [prod\_subscription\_name](#input\_prod\_subscription\_name) | PROD Subscription name | `string` | n/a | yes |
 | <a name="input_project_name_prefix"></a> [project\_name\_prefix](#input\_project\_name\_prefix) | Project name prefix (e.g. userregistry) | `string` | n/a | yes |
+| <a name="input_tlscert-api-portalefatturazione-pagopa-it"></a> [tlscert-api-portalefatturazione-pagopa-it](#input\_tlscert-api-portalefatturazione-pagopa-it) | n/a | `map` | <pre>{<br>  "pipeline": {<br>    "dns_record_name": "api",<br>    "dns_zone_name": "portalefatturazione.pagopa.it",<br>    "enable_tls_cert": true,<br>    "path": "TLS-Certificates",<br>    "variables": {<br>      "CERT_NAME_EXPIRE_SECONDS": "2592000"<br>    },<br>    "variables_secret": {}<br>  },<br>  "repository": {<br>    "branch_name": "refs/heads/master",<br>    "name": "le-azure-acme-tiny",<br>    "organization": "pagopa",<br>    "pipelines_path": "."<br>  }<br>}</pre> | no |
+| <a name="input_tlscert-portalefatturazione-pagopa-it"></a> [tlscert-portalefatturazione-pagopa-it](#input\_tlscert-portalefatturazione-pagopa-it) | n/a | `map` | <pre>{<br>  "pipeline": {<br>    "dns_record_name": "",<br>    "dns_zone_name": "portalefatturazione.pagopa.it",<br>    "enable_tls_cert": true,<br>    "path": "TLS-Certificates",<br>    "variables": {<br>      "CERT_NAME_EXPIRE_SECONDS": "2592000"<br>    },<br>    "variables_secret": {}<br>  },<br>  "repository": {<br>    "branch_name": "refs/heads/master",<br>    "name": "le-azure-acme-tiny",<br>    "organization": "pagopa",<br>    "pipelines_path": "."<br>  }<br>}</pre> | no |
 
 ## Outputs
 
