a26447532852b07f0550785894d042ddcbd9829a
trossibip <tommaso.rossi@bip-group.com>
2023-11-22 20:36:44 +0100

    refactor: Minor

--- src/azuredevops/core/00_service_connections_github.tf
+++ src/azuredevops/core/00_service_connections_github.tf
@@ -1,4 +1,18 @@
 
+module "secret_core" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3//key_vault_secrets_query?ref=v7.20.0"
+  providers = {
+    azurerm = azurerm.prod
+  }
+
+  resource_group = local.prod.key_vault_rg_name
+  key_vault_name = local.prod.key_vault_name
+
+  secrets = [
+    "azure-devops-github-ro-TOKEN",
+  ]
+}
+
 # Github service connection (read-only)
 resource "azuredevops_serviceendpoint_github" "azure-devops-github-ro" {
   project_id            = data.azuredevops_project.project.id
