47a4f9706e0b8bbf6cbaf80981afcff4e375575d
pp-ps <pietro.stroia@pagopa.it>
2023-11-23 12:07:17 +0100

    reuse the networking rg (in primary location)

--- src/core/00_vnet.tf
+++ src/core/00_vnet.tf
@@ -28,8 +28,8 @@ module "private_endpoint_snet" {
 module "secondary_vnet" {
   source              = "./.terraform/modules/__v3__/virtual_network/"
   name                = format("%s-secondary-vnet", local.project)
-  location            = azurerm_resource_group.secondary_networking.location
-  resource_group_name = azurerm_resource_group.secondary_networking.name
+  location            = azurerm_resource_group.networking.location
+  resource_group_name = azurerm_resource_group.networking.name
   # FIXME, move to variables
   ddos_protection_plan = {
     id     = "/subscriptions/0da48c97-355f-4050-a520-f11a18b8be90/resourceGroups/sec-p-ddos/providers/Microsoft.Network/ddosProtectionPlans/sec-p-ddos-protection"
@@ -44,7 +44,7 @@ module "private_endpoint_secondary_snet" {
   source                                    = "./.terraform/modules/__v3__/subnet/"
   name                                      = format("%s-%s-snet", local.project, "secondary_private_endpoint")
   address_prefixes                          = var.secondary_cidr_pvt_endp_snet
-  resource_group_name                       = azurerm_resource_group.secondary_networking.name
+  resource_group_name                       = azurerm_resource_group.networking.name
   virtual_network_name                      = module.secondary_vnet.name
   private_endpoint_network_policies_enabled = false
   service_endpoints                         = []
@@ -57,7 +57,7 @@ module "vnet_peering_between_primary_secondary" {
   source_virtual_network_name      = module.vnet.name
   source_remote_virtual_network_id = module.vnet.id
   source_allow_gateway_transit     = true # needed by vpn gateway for enabling routing from vnet to vnet_integration
-  target_resource_group_name       = azurerm_resource_group.secondary_networking.name
+  target_resource_group_name       = azurerm_resource_group.networking.name
   target_virtual_network_name      = module.secondary_vnet.name
   target_remote_virtual_network_id = module.secondary_vnet.id
   target_use_remote_gateways       = true # needed by vpn gateway for enabling routing from vnet to vnet_integration
