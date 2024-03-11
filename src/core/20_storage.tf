be3a030b0ffe44265497c3ca9fadc0e5fa520457
giannipapetti <43778383+giannipapetti@users.noreply.github.com>
2024-03-11 12:19:24 +0100

    modified:   src/core/20_storage.tf

--- src/core/20_storage.tf
+++ src/core/20_storage.tf
@@ -183,3 +183,52 @@ resource "azurerm_private_endpoint" "sap_storage_blob" {
 
   tags = var.tags
 }
+
+
+
+#
+# rel storage
+#
+#no public access, used by fatturazione API-BE
+module "rel_storage" {
+  source                               = "./.terraform/modules/__v3__/storage_account/"
+  name                                 = replace(format("%s-%s", local.project, "rel"), "-", "")
+  resource_group_name                  = azurerm_resource_group.analytics.name
+  location                             = var.secondary_location
+  account_kind                         = "StorageV2"
+  account_tier                         = "Standard"
+  access_tier                          = "Hot"
+  account_replication_type             = "ZRS"
+  blob_versioning_enabled              = true
+  blob_container_delete_retention_days = var.storage_delete_retention_days
+  allow_nested_items_to_be_public      = false
+  public_network_access_enabled        = false
+  tags                                 = var.tags
+}
+
+resource "azurerm_storage_container" "rel_rel" {
+  name                  = "rel"
+  storage_account_name  = module.rel_storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_private_endpoint" "rel_storage_blob" {
+  name                = format("%s-blob-endpoint", module.rel_storage.name)
+  location            = var.secondary_location
+  resource_group_name = azurerm_resource_group.analytics.name
+  subnet_id           = module.private_endpoint_secondary_snet.id
+
+  private_service_connection {
+    name                           = format("%s-blob-endpoint", module.rel_storage.name)
+    private_connection_resource_id = module.rel_storage.id
+    is_manual_connection           = false
+    subresource_names              = ["blob"]
+  }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.privatelink_blob_core_windows_net.id]
+  }
+
+  tags = var.tags
+}
