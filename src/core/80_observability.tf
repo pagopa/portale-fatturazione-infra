6205ec7411f44c844baf81b60ed6a5d90c23285d
tomrss <tommaso.rossi@pagopa.it>
2025-05-06 14:31:23 +0200

    feat: Diagnostic settings for app, func, agw

--- src/core/80_observability.tf
+++ src/core/80_observability.tf
@@ -1,4 +1,4 @@
-resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
+resource "azurerm_log_analytics_workspace" "law" {
   name                = format("%s-%s", local.project, "law")
   location            = azurerm_resource_group.monitoring.location
   resource_group_name = azurerm_resource_group.monitoring.name
@@ -14,6 +14,144 @@ resource "azurerm_application_insights" "application_insights" {
   location            = azurerm_resource_group.monitoring.location
   resource_group_name = azurerm_resource_group.monitoring.name
   application_type    = "other"
-  workspace_id        = azurerm_log_analytics_workspace.log_analytics_workspace.id
+  workspace_id        = azurerm_log_analytics_workspace.law.id
   tags                = var.tags
+}
+
+# Send AGW logs to LAW in non-prod (in prod a policy will take care of it)
+resource "azurerm_monitor_diagnostic_setting" "agw" {
+  count = var.env_short == "p" ? 0 : 1
+
+  name                           = azurerm_log_analytics_workspace.law.name
+  target_resource_id             = module.agw.id
+  log_analytics_workspace_id     = azurerm_log_analytics_workspace.law.id
+  log_analytics_destination_type = "Dedicated"
+
+  enabled_log {
+    category = "ApplicationGatewayAccessLog"
+  }
+  enabled_log {
+    category = "ApplicationGatewayFirewallLog"
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = false
+  }
+}
+
+# Send API App Service logs to LAW in non-prod (in prod a policy will take care of it)
+resource "azurerm_monitor_diagnostic_setting" "app_api" {
+  count = var.env_short == "p" ? 0 : 1
+
+  name                           = azurerm_log_analytics_workspace.law.name
+  target_resource_id             = azurerm_linux_web_app.app_api.id
+  log_analytics_workspace_id     = azurerm_log_analytics_workspace.law.id
+  log_analytics_destination_type = "Dedicated"
+
+  enabled_log {
+    category = "AppServiceConsoleLogs"
+  }
+  enabled_log {
+    category = "AppServiceHTTPLogs"
+  }
+  enabled_log {
+    category = "AppServiceAuditLogs"
+  }
+  enabled_log {
+    category = "AppServiceAppLogs"
+  }
+  enabled_log {
+    category = "AppServicePlatformLogs"
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = false
+  }
+
+  lifecycle {
+    # it keeps getting changed
+    ignore_changes = [log_analytics_destination_type]
+  }
+}
+
+# Send FE App Service logs to LAW in non-prod (in prod a policy will take care of it)
+resource "azurerm_monitor_diagnostic_setting" "app_fe" {
+  count = var.env_short == "p" ? 0 : 1
+
+  name                           = azurerm_log_analytics_workspace.law.name
+  target_resource_id             = azurerm_linux_web_app.app_fe.id
+  log_analytics_workspace_id     = azurerm_log_analytics_workspace.law.id
+  log_analytics_destination_type = "Dedicated"
+
+  enabled_log {
+    category = "AppServiceConsoleLogs"
+  }
+  enabled_log {
+    category = "AppServiceHTTPLogs"
+  }
+  enabled_log {
+    category = "AppServiceAuditLogs"
+  }
+  enabled_log {
+    category = "AppServiceAppLogs"
+  }
+  enabled_log {
+    category = "AppServicePlatformLogs"
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = false
+  }
+
+  lifecycle {
+    # it keeps getting changed
+    ignore_changes = [log_analytics_destination_type]
+  }
+}
+
+# Send API Function App logs to LAW
+resource "azurerm_monitor_diagnostic_setting" "func_api" {
+  name                           = azurerm_log_analytics_workspace.law.name
+  target_resource_id             = azurerm_linux_function_app.api.id
+  log_analytics_workspace_id     = azurerm_log_analytics_workspace.law.id
+  log_analytics_destination_type = "Dedicated"
+
+  enabled_log {
+    category = "FunctionAppLogs"
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = false
+  }
+
+  lifecycle {
+    # it keeps getting changed
+    ignore_changes = [log_analytics_destination_type]
+  }
+}
+
+# Send integration Function App logs to LAW
+resource "azurerm_monitor_diagnostic_setting" "func_integration" {
+  name                           = azurerm_log_analytics_workspace.law.name
+  target_resource_id             = azurerm_linux_function_app.integration.id
+  log_analytics_workspace_id     = azurerm_log_analytics_workspace.law.id
+  log_analytics_destination_type = "Dedicated"
+
+  enabled_log {
+    category = "FunctionAppLogs"
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = false
+  }
+
+  lifecycle {
+    # it keeps getting changed
+    ignore_changes = [log_analytics_destination_type]
+  }
 }
